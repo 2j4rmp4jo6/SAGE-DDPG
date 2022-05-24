ab7d077843419c5fb12287f0d3cdd8cb8ff94efb
2j4rmp4jo6 <50002701+2j4rmp4jo6@users.noreply.github.com>
2022-05-24 15:13:54 +0800

    test reward

--- FL_env.py
+++ FL_env.py
@@ -174,7 +174,7 @@ class FL_env():
                 else:
                     good_to_bad += 1
         # reward function
-        reward = (good_to_good * ((1 - 0.4) / (1 - f.attack_ratio)) + bad_to_bad * (0.4 / f.attack_ratio)) * (0.9 ** action[2]) * (0.9 ** round)
+        reward = (good_to_good * ((1 - 0.4) / (1 - f.attack_ratio)) + bad_to_bad * (0.4 / f.attack_ratio) * 4) * (0.8 ** action[2]) * (0.8 ** round)
 
         # 中止條件
         if round > 20 or len(self.my_groups.intermediate) == 0:
